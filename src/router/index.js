2c1e1d00d16919471a41322fee69019497408036
CarlosCasimiroF <102533234+CarlosCasimiroF@users.noreply.github.com>
2024-05-04 09:09:38 -0500

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,16 @@
 import {createRouter, createWebHistory} from "vue-router";
 import PromosComponent from "@/tracking/pages/promos.component.vue";
 import HistoryComponent from "@/tracking/pages/history.component.vue";
+import ConfigComponent from "@/tracking/pages/config.component.vue";
+import SupportComponent from "@/tracking/pages/support.component.vue";
 
 const router  = createRouter({
     history: createWebHistory(),
     routes : [
         {path: '/promos', component: PromosComponent, meta: {title: 'Promociones'}},
         {path: '/history', component: HistoryComponent, meta: {title: 'Historial de viajes'}},
+        {path:'/config',component: ConfigComponent,meta: {title: 'Configuracion'}},
+        {path:'/support',component: SupportComponent,meta: {title: 'Support'}},
         {path: '/', redirect: '/promos'},
     ],
 });
@@ -15,7 +19,7 @@ router.beforeEach((to,
                    from,
                    next) => {
     let baseTitle = 'TrackMyRoute1';
-    document.title = `${baseTitle} | ${to.meta['title']}`;
+    document.title = ${baseTitle} | ${to.meta['title']};
     next();
 });
 
