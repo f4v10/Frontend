ade3572187347c11fd87db2cb91da6de81b1b03d
CarlosCasimiroF <102533234+CarlosCasimiroF@users.noreply.github.com>
2024-05-04 10:00:50 -0500

    Update config.component.vue

--- src/tracking/pages/config.component.vue
+++ src/tracking/pages/config.component.vue
@@ -1,94 +1,120 @@
-<script >
-export default {
-  name:'side-bar.component',
-  title:'SideBar',
-  data() {
-    return {
-      drawer:false,
-      items:[
-        {label:'Notificaciones', to: '/promos'},
-        {label:'Buscar rutas', to: '/promos'},
-        {label:'Pagar pasaje', to: '/promos'},
-        {label:'Promociones', to: '/promos'},
-        {label:'Historial de viajes', to: '/history'},
-        {label:'Soporte y ayuda', to: '/support'},
-        {label:'Configuración', to: '/config'},
-      ]
-    };
-  },
-  methods: {
-    toggleDrawer() {
-      this.drawer = !this.drawer;
-    }
-  }
-}
-</script>
-
 <template>
-  <div class="container">
-    <pv-sidebar :visible="true" :showCloseIcon="false" class="p-sidebar-sm">
-      <template #header>
-        <h3 class="sidebar-title">TrackMyRoute</h3>
-      </template>
-      <ul class="p-list-none">
-        <router-link v-for="item in items" :key="item.label"
-                     v-slot="{navigate,href}" :to="item.to" custom>
-          <li class="p-p-2 p-d-flex p-flex-row p-ai-center p-jc-between">
-            <pv-button  :href="href" class="p-button-rounded p-button-text p-button-plain p-button-success"  @click="navigate" >
-              {{item.label}}
-            </pv-button>
-          </li>
-        </router-link>
-      </ul>
-    </pv-sidebar>
+  <div class="container-config">
+    <div class="column-left">
+      <pv-card style="width: 100%; overflow: hidden" class="bg-white">
+        <template #title>
+          <h1 class="titles">Configuración</h1>
+        </template>
+        <template #content>
+          <img alt="profile" src="../../../public/images/profile.png" class="profile-image"/>
+        </template>
+        <template #footer>
+          <h1 class="titles">Plan Actual Free</h1>
+          <pv-button class="button-plan" >
+            Cambiar Plan
+          </pv-button>
+        </template>
+      </pv-card>
+    </div>
+    <div class="column-right">
+      <div class="input-container">
+        <span class="text-input">Nombre</span>
+        <button class="icon-button">
+          <i class="pi pi-pencil"></i>
+        </button>
+      </div>
+      <div class="input-container">
+        <span class="text-input">Apellido</span>
+        <button class="icon-button">
+          <i class="pi pi-pencil"></i>
+        </button>
+      </div>
+      <div class="input-container">
+        <span class="text-input">Email</span>
+        <button class="icon-button">
+          <i class="pi pi-pencil"></i>
+        </button>
+      </div>
+    </div>
   </div>
-  <router-view></router-view>
 </template>
 
-<style scoped>
-
-.container{
-  height: 100%;
+<style>
+.container-config {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  border-radius: 2%;
+  overflow: hidden;
+  width: 50vw;
+  height: 50vh;
 }
 
-.sidebar-title{
-  text-align: center;
-  font-size: 32px;
+.column-left, .column-right {
+  background: #FFF;
+
 }
 
-ul li{
-  list-style: none;
-  margin: 50px
+.titles {
+  color: black; /* Color del texto */
+  font-family: "Montserrat", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: bold;
+  font-style: normal;
+  font-size:40px;
+}
+.text-input{
+  color: black; /* Color del texto */
+  font-family: "Montserrat", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: bold;
+  font-style: normal;
+}
+.profile-image {
+  width: 50%; /* Reducción del tamaño de la imagen en un 50% */
+  display: block; /* Asegura que la imagen no tenga espacio extra alrededor */
+  margin: auto; /* Centra la imagen horizontalmente dentro del contenedor */
 }
 
-.p-sidebar-sm {
-  width: 200px;
+.column-right {
+  display: flex;
+  flex-direction: column;
+  justify-content: center; /* Centra el contenido verticalmente */
+  align-items: center; /* Centra el contenido horizontalmente */
+}
+.input-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 8px;
+  background-color: #D9D9D9;
+  border-radius: 4px;
+  width:70%;
+  margin-bottom: 20px;
 }
 
-li Button{
-  width: 300px;
-  height: 50px;
+.input-container span {
+  font-size: 30px;
 }
 
-.p-button-success {
-  background-color: #4CAF50;
-  color: white;
+.icon-button {
+  background: none;
   border: none;
-  border-radius: 10px;
-  padding: 10px;
   cursor: pointer;
-  transition: background-color 0.3s ease;
+  display: flex;
+  align-items: center;
+  color:black;
+}
+.button-plan {
+  background-color: #0066FF;
+  color: white;
+  font-family: "Montserrat", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: bold;
+  font-style: normal;
+  font-size:25px;
+  width:35%;
 }
 
-.p-button-success:hover {
-  background-color: #3e8e41;
-}
-
-.p-button-success:focus {
-  outline: none;
-}
-
-.p-button-success:active {
-  background-color: #2e7d32;
-}
 </style>
+<script setup lang="ts">
+</script>
