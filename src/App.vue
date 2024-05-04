4119f514426ebcb93fd17bca8deae143e49b8bd4
ArianRM <arianramirezmendoza@gmail.com>
2024-05-04 05:44:10 -0500

    feat: pay tickets view

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <script >
   import SideBar from "@/public/pages/side-bar.component.vue";
   import SearchRoutes from "@/tracking/pages/search-routes.component.vue"
+  import PayTicket from "@/tracking/pages/pay-ticket.component.vue"
   export default {
     name: "App",
     title: "TrackMyRoute2",
-    components: {SearchRoutes, SideBar},
+    components: {SearchRoutes, SideBar, PayTicket},
   }
 </script>
 
 <template>
-  <div class="app-container">
+  <div class="">
     <div class="sidebar-container">
-<!--      <side-bar></side-bar>-->
+      <side-bar></side-bar>
     </div>
     <div class="main-container">
-      <search-routes></search-routes>
+<!--      <search-routes></search-routes>-->
+      <pay-ticket></pay-ticket>
     </div>
   </div>
 </template>
