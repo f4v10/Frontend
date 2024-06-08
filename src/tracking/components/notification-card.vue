c4b621e56f317128a448cbb74f3761762ec65a1b
f4v10 <102195498+f4v10@users.noreply.github.com>
2024-06-08 13:46:42 -0500

    feat(notifications-components): added components to bounded context notifications.

---
+++ src/tracking/components/notification-card.vue
@@ -0,0 +1,29 @@
+<script>
+import {Notification} from "@/tracking/model/notifications/notification.entity.js";
+export default {
+  name: "promo-card",
+  props: {nots:Notification}
+}
+</script>
+
+<template>
+  <pv-card class="p-col-4 mt-4 mb-4 promo-card">
+    <template #header>
+      <p class="m-0"> Title: {{ nots.title }} </p>
+    </template>
+    <template #content>
+      <p class="m-0"> Description: {{ nots.description }} </p>
+    </template>
+  </pv-card>
+</template>
+
+<style scoped>
+.promo-card {
+  width: 50%;
+  margin: auto;
+  text-align: center;
+}
+img {
+  width: 100%;
+}
+</style>
